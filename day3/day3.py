252559c50313e591f6cda46a6e6b974284e630a3
rslavin <rocky.slavin@gmail.com>
2018-12-04 13:00:49 -0600

    made day 3 much faster

--- day3/day3.py
+++ day3/day3.py
@@ -1,12 +1,12 @@
 import sys
 import re
 
-f = [[0] * 1000] * 1000
+f = [[0] * 1000 for i in range(1000)]
 
 for line in sys.stdin:
     m = re.search(r".+@ (\d+),(\d+): (\d+)x(\d+)", line)
-    for p, x in enumerate(f):
-        if int(m.group(1)) <= p < int(m.group(1)) + int(m.group(3)):
-            f[p] = ([(v + 1 if int(m.group(2)) <= i < int(m.group(2)) + int(m.group(4)) else v) for i,v in enumerate(x)])
+    for p in range(int(m.group(1)), int(m.group(1)) + int(m.group(3))):
+            f[p][int(m.group(2)):int(m.group(2))+int(m.group(4))] = \
+                [v + 1 for v in f[p][int(m.group(2)):int(m.group(2)) + int(m.group(4))]]
 
 print(sum(1 if y > 1 else 0 for x in f for y in x))
--- day3/day3_2.py
+++ day3/day3_2.py
@@ -1,21 +1,21 @@
 import sys
 import re
 
-fabric = [[0] * 1000] * 1000
+f = [[0] * 1000 for i in range(1000)]
 
 lines = sys.stdin.readlines()
 
 for line in lines:
     m = re.search(r".+@ (\d+),(\d+): (\d+)x(\d+)", line)
-    for p, x in enumerate(fabric):
+    for p in range(int(m.group(1)), int(m.group(1)) + int(m.group(3))):
         if int(m.group(1)) <= p < int(m.group(1)) + int(m.group(3)):
-            fabric[p] = (
-            [(v + 1 if int(m.group(2)) <= i < int(m.group(2)) + int(m.group(4)) else v) for i, v in enumerate(x)])
+            f[p][int(m.group(2)):int(m.group(2)) + int(m.group(4))] = \
+                [v + 1 for v in f[p][int(m.group(2)):int(m.group(2)) + int(m.group(4))]]
 
 for line in lines:
     m = re.search(r"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)", line)
     n = False
-    for x in fabric[int(m.group(2)):int(m.group(2)) + int(m.group(4))]:
+    for x in f[int(m.group(2)):int(m.group(2)) + int(m.group(4))]:
         if sum(1 for y in x[int(m.group(3)):int(m.group(3)) + int(m.group(5))] if y > 1):
             n = True
             break

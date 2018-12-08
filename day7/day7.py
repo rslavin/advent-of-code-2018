f3af26f6ea1ebc701a82fc0ad46c463f021565ca
rslavin <rocky.slavin@gmail.com>
2018-12-07 20:58:58 -0600

    working, sorting is wrong for ties (Should be VY, not YV

--- day7/day7.py
+++ day7/day7.py
@@ -8,31 +8,39 @@ for line in sys.stdin:
         children[mo.group(1)] = []
     children[mo.group(1)].append(mo.group(2))
 
-map(lambda c: sorted(c), children)
+# map(lambda c: sorted(c), children)
 
-ordered = []
 end = None
+ordered = []
+roots = sorted([x for x in children.keys() if x not in [y for sublist in children.values() for y in sublist]])
 def find_order(root):
     print(f"root: {root}")
-    if root not in children.keys():
-        print(f"skipping: {root}")
-        global end
-        end = root
-        return()
-    ordered.append(root) # maybe move this
-    for n in children[root]:
-        # if all of its parents are in ordered
-        if n in children.keys() and not [p for p in children.keys() if p in ordered and n in p]:
+    if root not in ordered:
+        print(f"adding {root}")
+        ordered.append(root)
+    for n in sorted(children[root]):
+        print(n)
+        print([p for p in children.keys() if p not in ordered and p not in roots and n in children[p]])
+        if n not in children.keys():
+            global end
+            end = n
+        if n in children.keys() and not [p for p in children.keys() if p not in ordered and n in children[p]]:
             print(f"checking: {n}") # these are getting skipped in ordered
             # ordered.append(n) # appending it doesn't help
-            # find_order(n)
-            for m in children[n]:
-                find_order(m)
+            # if parents in ordered, only call find_order if there are no parents before it
+            find_order(n)
+            # for m in children[n]:
+            #     find_order(m)
         # else:
         #     ordered.append(m)
+    print("----------------------")
 
+for p in children.keys():
+    print(f"{p} : {sorted(children[p])}")
 
-find_order(next(iter(children)))
+for y in roots:
+    print(y)
+    find_order(y)
 print("".join(ordered) + end)
 
 

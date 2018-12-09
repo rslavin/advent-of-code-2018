dc2b229fd6c28fc714962649d24591576fb17cff
rslavin <rocky.slavin@gmail.com>
2018-12-09 11:50:47 -0600

    completed day 7

--- day7/day7_2.py
+++ day7/day7_2.py
@@ -1,36 +1,43 @@
-import day7
+import sys
+import re
 
-steps, children = day7.find_order()
+children = {}
+steps = set()
+for line in sys.stdin:
+    mo = re.search(r"Step (.).*(.) can", line)
+    if mo.group(1) not in children:
+        children[mo.group(1)] = []
+    children[mo.group(1)].append(mo.group(2))
+    steps.add(mo.group(2))
 
-time = 0
 complete = []
-roots = [x for x in children.keys() if x not in [y for sublist in children.values() for y in sublist]]
+queue = sorted(list(steps) + [x for x in children if x not in [y for sublist in children.values() for y in sublist]])
+ready = []
+
+i = 0
+time = 0
 workers = [0] * 5
-slots = [None] * 5
-while len(steps):
-    print(steps)
-    for i in range(len(workers)):
-        # if worker is free and step[0] has complete dependencies
-        if workers[i] == 1:
-            print(f"completing {slots[i]}")
-            complete.append(slots[i]) # can't use pop here because we need to preserve length/order
-            slots[i] = None
-        print(f"testing {steps[0]}: parents: { [p for p in children if (p not in complete or p not in roots) and steps[0] in children[p]]}")
-        if not workers[i] and len(steps) and (steps[0] in roots or not [p for p in children if (p not in complete or p not in roots) and steps[0] in children[p]]):
-            print(f"adding {steps[0]} ({ord(steps[0].lower()) - 36}) to worker")
-            slots[i] = steps[0]
-            workers[i] = ord(steps.pop(0).lower()) - 36
+slots = [""] * 5
+while True:
+    for w in range(len(workers)):
+        if not workers[w] and len(ready):
+            ready.sort()
+            workers[w] = ord(ready[0].lower()) - 36
+            slots[w] = ready.pop(0)
+        if workers[w] == 1:
+            complete.append(slots[w])
+            slots[w] = ""
 
-    time += 1
-    print(workers)
+    while i < len(queue):
+        step = queue[i]
+        i += 1
+        if step not in ready + complete + slots and not [p for p in children if p not in complete and step in children[p]]:
+            ready.append(step)
+            i = 0
+    if not len(ready) and not sum(workers):
+        break
     workers = list(map(lambda x: x - 1 if x > 0 else x, workers))
-    print(workers)
-    if sum(workers) ==\
-            0:
-        exit()
+    time += 1
+    i = 0
 
-
-print(time)
-time += max(workers)
-
-print(time)
+print(time + max(workers) - 1)

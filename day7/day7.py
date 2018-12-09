32a9d32568b309d181ed6f27ebd41a687f703cfb
rslavin <rocky.slavin@gmail.com>
2018-12-08 21:15:46 -0600

    completed day 7 part 1

--- day7/day7.py
+++ day7/day7.py
@@ -2,50 +2,28 @@ import sys
 import re
 
 children = {}
+steps = set()
 for line in sys.stdin:
     mo = re.search(r"Step (.).*(.) can", line)
     if mo.group(1) not in children:
         children[mo.group(1)] = []
     children[mo.group(1)].append(mo.group(2))
+    steps.add(mo.group(2))
 
-# map(lambda c: sorted(c), children)
+complete = []
+ready = sorted([x for x in children.keys() if x not in [y for sublist in children.values() for y in sublist]])
+complete.append(ready.pop(0))
+queue = sorted(list(steps) + ready)
 
-end = None
-ordered = []
-roots = sorted([x for x in children.keys() if x not in [y for sublist in children.values() for y in sublist]])
-def find_order(root):
-    print(f"root: {root}")
-    if root not in ordered:
-        print(f"adding {root}")
-        ordered.append(root)
-    for n in sorted(children[root]):
-        print(n)
-        print([p for p in children.keys() if p not in ordered and p not in roots and n in children[p]])
-        if n not in children.keys():
-            global end
-            end = n
-        if n in children.keys() and not [p for p in children.keys() if p not in ordered and n in children[p]]:
-            print(f"checking: {n}") # these are getting skipped in ordered
-            # ordered.append(n) # appending it doesn't help
-            # if parents in ordered, only call find_order if there are no parents before it
-            find_order(n)
-            # for m in children[n]:
-            #     find_order(m)
-        # else:
-        #     ordered.append(m)
-    print("----------------------")
-
-for p in children.keys():
-    print(f"{p} : {sorted(children[p])}")
-
-for y in roots:
-    print(y)
-    find_order(y)
-print("".join(ordered) + end)
-
-
-# for last node, when you check for if you need to do the node,
-# if it has no entry in children, it is the last one in the branch
-# (recurse back up)
-
-
+i = 0
+while i < len(queue):
+    step = queue[i]
+    i += 1
+    if step in ready or not [p for p in children.keys() if p not in complete and step in children[p]]:
+        if step not in ready + complete:
+            ready.append(step)
+        queue.remove(step)
+        i = 0
+        ready.sort()
+        complete.append(ready.pop(0))
+print("".join(complete))
